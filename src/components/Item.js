1f626f09dcc39e0f5a537f9fc4d52df4255e71bc
Abigail Arisa <110755503+Abigailarisa@users.noreply.github.com>
2022-10-19 00:43:56 -0500

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setInCart] = useState(false);
+
+  function handleAddToCartClick() {
+    setInCart((isInCart) =>!isInCart)
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : "" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "Remove From Cart" : "add"} onClick={handleAddToCartClick}>{isInCart ? "Remove" : "Add"} to Cart</button>
     </li>
   );
 }
